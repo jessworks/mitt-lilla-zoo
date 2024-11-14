6b00b6cd4355035b74490e85a788b6c6843cc612
jessworks <48979913+jessworks@users.noreply.github.com>
2024-11-14 16:21:51 +0100

    feat: Animal.tsx: button click shows message and disables button

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -6,26 +6,30 @@ import { getAnimalLS } from '../services/animalsService';
 
 export const Animal = () => {
     const { id } = useParams<{ id:string }>();
-  const [animal, setAnimal] = useState<IAnimal | undefined>(undefined);
+    const [animal, setAnimal] = useState<IAnimal | undefined>(undefined);
+    const [animalFed, setAnimalFed] = useState(false);
+    const [btnDisabled, setBtnDisabled] = useState(false);
   
-  useEffect(() => {
-    if (id) {
-        const animalFromLS = getAnimalLS(Number(id));
-        setAnimal(animalFromLS);
-    }
-  }, [id]);
+    useEffect(() => {
+        if (id) {
+            const animalFromLS = getAnimalLS(Number(id));
+            setAnimal(animalFromLS);
+        }
+    }, [id]);
 
-  const handleFeedClick = () => {
-    console.log("feed click");
-  };
+    const handleFeedClick = () => {
+        setAnimalFed(true);
+        setBtnDisabled(true);
+    };
 
-  return (
+    return (
         <>
             {animal ? (
             <>
                 <h1>{animal.name}</h1>
                 <img src={animal.imageUrl} alt={animal.name} style={{ width: "200px", height: "auto" }} />
-                <button onClick={handleFeedClick}>Feed</button>
+                <button onClick={handleFeedClick} disabled={btnDisabled}>Feed</button>
+                {animalFed && <p>{animal.name} was fed.</p>}
             </>
             ) : (
                 <h1>Animal not found</h1>
