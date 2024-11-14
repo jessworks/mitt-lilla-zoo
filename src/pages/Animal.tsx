4e167e5d3844ef0204145ffcab66a2d26bf3633e
jessworks <48979913+jessworks@users.noreply.github.com>
2024-11-14 16:44:23 +0100

    feat: Animals.tsx: message shows time of feeding

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -9,6 +9,7 @@ export const Animal = () => {
     const [animal, setAnimal] = useState<IAnimal | undefined>(undefined);
     const [animalFed, setAnimalFed] = useState(false);
     const [btnDisabled, setBtnDisabled] = useState(false);
+    const [timeFed, setTimeFed] = useState(0);
   
     useEffect(() => {
         if (id) {
@@ -20,6 +21,7 @@ export const Animal = () => {
     const handleFeedClick = () => {
         setAnimalFed(true);
         setBtnDisabled(true);
+        setTimeFed(new Date().toLocaleDateString([], { hour: '2-digit', minute: '2-digit' }));
     };
 
     return (
@@ -29,7 +31,7 @@ export const Animal = () => {
                 <h1>{animal.name}</h1>
                 <img src={animal.imageUrl} alt={animal.name} style={{ width: "200px", height: "auto" }} />
                 <button onClick={handleFeedClick} disabled={btnDisabled}>Feed</button>
-                {animalFed && <p>{animal.name} was fed.</p>}
+                {animalFed && <p>{animal.name} was fed {timeFed}.</p>}
             </>
             ) : (
                 <h1>Animal not found</h1>
