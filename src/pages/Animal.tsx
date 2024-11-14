248ba4c750c27c2ee6680832bdd6f358bc7f429f
jessworks <48979913+jessworks@users.noreply.github.com>
2024-11-14 16:56:06 +0100

    feat: Animal.tsx: return button showing 'Home' page

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { IAnimal } from "../models/IAnimal";
 import { getAnimalLS } from '../services/animalsService';
 
@@ -10,6 +11,7 @@ export const Animal = () => {
     const [animalFed, setAnimalFed] = useState(false);
     const [btnDisabled, setBtnDisabled] = useState(false);
     const [timeFed, setTimeFed] = useState(0);
+    const navigate = useNavigate();
   
     useEffect(() => {
         if (id) {
@@ -24,14 +26,19 @@ export const Animal = () => {
         setTimeFed(new Date().toLocaleDateString([], { hour: '2-digit', minute: '2-digit' }));
     };
 
+    const handleBackToZooClick = () => {
+        navigate(`/`);
+    }
+
     return (
         <>
             {animal ? (
             <>
                 <h1>{animal.name}</h1>
                 <img src={animal.imageUrl} alt={animal.name} style={{ width: "200px", height: "auto" }} />
-                <button onClick={handleFeedClick} disabled={btnDisabled}>Feed</button>
+                <button onClick={handleFeedClick} disabled={btnDisabled}>Feed Me</button>
                 {animalFed && <p>{animal.name} was fed {timeFed}.</p>}
+                <button onClick={handleBackToZooClick}>Back to Zoo</button>
             </>
             ) : (
                 <h1>Animal not found</h1>
